83c8e48de86125d31ccb23907d06a1a9c40b32d7
Mikhail <milovidovmikhail@gmail.com>
2019-08-02 01:19:42 +0300

    Fixed install buttons, fixrd sitemap. Added robots.txt

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,25 @@
 // This is where it all goes :)
+
+new ClipboardJS('.copy-button');
+
+
+let OSName = 'unknown';
+if (navigator.appVersion.indexOf("Win") != -1) OSName = 'windows';
+if (navigator.appVersion.indexOf("Mac") != -1) OSName = 'macos';
+if (navigator.appVersion.indexOf("X11") != -1) OSName = 'linux';
+if (navigator.appVersion.indexOf("Linux") != -1) OSName = 'Linux';
+
+switch (OSName) {
+    case 'unknown':
+    case 'linux':
+        $('#install_buttons a[href="#install_linux"]').trigger('click');
+        break;
+    case 'macos':
+        $('#install_buttons a[href="#install_macos"]').trigger('click')
+        break;
+    case 'windows':
+        $('#install_buttons a[href="#install_windows"]').trigger('click')
+        break;
+
+    default:;
+}
